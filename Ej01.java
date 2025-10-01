d5d6bd8fd33d7f5ad0e9c05a75f69b5248b295a6
lcfxr <nerea.garcia.vergara.al@iespoligonosur.org>
2025-10-01 14:41:32 +0000

    Terminado Ej01

---
+++ Ej01.java
@@ -0,0 +1,5 @@
+public class Ej01 {
+    public static void main (String[]args){
+        System.out.println ("Nerea");
+    }
+}
---
+++ Ej02.java
@@ -0,0 +1,6 @@
+public class Ej02 {
+    public static void main (String[]args){
+        System.out.println ("Nerea");
+        System.out.println ("27");
+    }
+}
