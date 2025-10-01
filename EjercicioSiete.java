48ee2fa06ca2f378f407165ece4e96204575b6da
lcfxr <nerea.garcia.vergara.al@iespoligonosur.org>
2025-10-01 17:09:08 +0000

    Correccion Ej07

---
+++ EjercicioSiete.java
@@ -0,0 +1,12 @@
+public class EjercicioSiete{
+    public static void main (String[]args){
+
+        //VARIABLES CON CAMEL CASE
+        int numeroUno = 3;
+        int numeroDos = 8;
+        int sumaUnoDos = numeroUno+numeroDos;
+
+        System.out.println ("Las variables son " +numeroUno  +numeroDos  +sumaUnoDos);
+    
+    }
+}
