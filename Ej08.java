8aa6c03ca7ea1a04c55525690e019e045782128c
lcfxr <nerea.garcia.vergara.al@iespoligonosur.org>
2025-10-06 17:43:52 +0000

    Ej08 resuelto

---
+++ Ej08.java
@@ -0,0 +1,10 @@
+public class Ej08{
+    public static void main (String[]args){
+        int num1=3;
+        double num2=4.2;
+        char fraseuno= "Hola";
+        boolean metira;
+
+        System.out.println("Los valores son " +num1+ "," +num2+ "," +fraseuno+ " y " +mentira);
+    }
+}
