323002f274917d7e6265f1280dbd93818b44f1ff
lcfxr <nerea.garcia.vergara.al@iespoligonosur.org>
2025-10-07 16:52:38 +0000

    Ej13 resuelto

---
+++ Ej13.java
@@ -0,0 +1,9 @@
+public class Ej13{
+    public static void main (String[]args){
+         //DECLARAMOS LA CONSTANTE QUE SERA 0.15
+         double irpf = 0.15;
+
+         System.out.println ("El sueldo bruto es de 1500 y con el IRPF se queda en " +(1500-(irpf*1500)));
+
+    }
+}
