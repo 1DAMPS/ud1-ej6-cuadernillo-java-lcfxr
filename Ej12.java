b28ab1610c181cd2c5f7fee222e12276fa578eba
lcfxr <nerea.garcia.vergara.al@iespoligonosur.org>
2025-10-07 16:46:57 +0000

    Ej12 resuelto

---
+++ Ej12.java
@@ -0,0 +1,9 @@
+public class Ej12{
+    public static void main (String[]args){
+
+        //LA VARIABLE FLOAT ES DE 6 A 7 CIFRAS DECIMALES Y TIENES QUE PONER UNA F AL FINAL PORQUE SI NO TE SALE ERROR
+        float num = 3.1416F;
+
+        System.out.println("El numero PI es " +num);
+    }
+}
