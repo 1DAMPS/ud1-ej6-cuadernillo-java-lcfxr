37ab1b3589038f775c5465d1648c85d97c4b0dfd
lcfxr <nerea.garcia.vergara.al@iespoligonosur.org>
2025-10-07 17:10:10 +0000

    Ej18 resuelto

---
+++ Ej18.java
@@ -0,0 +1,16 @@
+/**
+ * @author Nerea Garcia Vergara
+ * fecha 07/10/2025
+ * Esto es para crear un java doc con toda la informacion del codigo
+ */
+public class Ej18{
+    public static void main (String[]args){
+        //CREAMOS VARIABLE
+        int num = 5;
+
+        /*LO MOSTRAMOS POR PANTALLA
+        ASI QUE QUEDE BONITO*/
+
+        System.out.println("El numero es " +num);
+    }
+}
