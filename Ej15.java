a6f3c31834cef2fc405675a6b214f33ed2bbd79b
lcfxr <nerea.garcia.vergara.al@iespoligonosur.org>
2025-10-07 16:59:36 +0000

    Ej15 resuelto

---
+++ Ej15.java
@@ -0,0 +1,11 @@
+public class Ej15{
+    public static void main(String[]args){
+        int num = 2147483647;
+        int suma = 2147483647+1;
+
+        System.out.println("La suma es de " +suma);
+
+        //EL ERROR LO QUE TE HACE ES QUE TE SALGA EL NUMERO EN NEGATIVO PORQUE EL INT SOLO LLEGA HASTA EL 2147483648 
+
+    }
+}
