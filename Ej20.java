20efd3545b502a8dff6911859a219ed016f5f1e9
lcfxr <nerea.garcia.vergara.al@iespoligonosur.org>
2025-10-07 17:15:45 +0000

    Ej20 resuelto

---
+++ Ej20.java
@@ -0,0 +1,13 @@
+/**
+ * El programa realiza la multiplicacion para calcular el area de un cuadrado
+ * @author Nerea Garcia Vergara
+ * @version 1.0 
+ * */ 
+
+public class Ej20{
+    public static void main (String[]args){
+        int radio = 5; //CREA LA VARIABLE RADIO
+        double area = 3.1416 * radio * radio; //REALIZA LA OPERACION DE CREACION DEL AREA
+        System.out.println(area); //MUESTRA POR PANTALLA EL AREA
+    }
+}
