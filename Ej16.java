1a5442543f33ed40e16c118f8cdc3fab40993db1
lcfxr <nerea.garcia.vergara.al@iespoligonosur.org>
2025-10-07 17:02:53 +0000

    Ej16 resuelto

---
+++ Ej16.java
@@ -0,0 +1,10 @@
+public class Ej16{
+    public static void main (String[]args){
+        short num = 30000;
+        short multi = 30000*10;
+
+        System.out.println("La multiplicacion es de " +multi);
+
+        //ME APARECE QUE UN SHORT NO SE PUEDE MULTIPLICAR POR UN INT
+    }
+}
