2b84addbcebd8763fc4c414835bedca210e50604
lcfxr <nerea.garcia.vergara.al@iespoligonosur.org>
2025-10-07 16:39:26 +0000

    Ej11 resuelto

---
+++ Ej11.java
@@ -0,0 +1,11 @@
+public class Ej11{
+    public static void main (String[]args){
+
+        //EL LONG ES DE +- 9 TRILLONES. ALFINAL HAY QUE PONERLE UNA L
+        long mundo = 8000000000L;
+
+        System.out.println ("El mundo tiene " +mundo+ " habitantes");
+
+
+    }
+}
