fa38bf783b29512958149adcd06112aa86fe460e
lcfxr <nerea.garcia.vergara.al@iespoligonosur.org>
2025-10-07 16:35:08 +0000

    Ej10 resuelto

---
+++ Ej10.java
@@ -0,0 +1,10 @@
+public class Ej10{
+    public static void main (String[]args){
+
+        //AÑADIMOS LAS VARIABLES. BYTE ES DESDE -128 A 127 Y SHORT ES DESDE -32.768 HASTA 32.767
+        byte num1 = 20;
+        short num2 = 3;
+
+        System.out.println("Los numeros son: " +num1+ " y " +num2);
+    }
+}
