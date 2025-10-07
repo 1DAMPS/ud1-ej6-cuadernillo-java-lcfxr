3f52e6ce549895cbf3873424cd73ac1706ef1a8d
lcfxr <nerea.garcia.vergara.al@iespoligonosur.org>
2025-10-07 17:12:20 +0000

    Ej19 resuelto

---
+++ Ej19.java
@@ -0,0 +1,7 @@
+public class Ej19{
+    public static void main (String[]args){
+        int radio = 5; //CREA LA VARIABLE RADIO
+        double area = 3.1416 * radio * radio; //REALIZA LA OPERACION DE CREACION DEL AREA
+        System.out.println(area); //MUESTRA POR PANTALLA EL AREA
+    }
+}
